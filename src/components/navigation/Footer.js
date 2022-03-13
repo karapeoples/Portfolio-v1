6e2241fd9d15f3e5836ff0e209c1ebef47b32526
Kara <karapeoples.dev@gmail.com>
2022-03-13 06:26:52 -0500

    Fixed Email Button

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -4,41 +4,41 @@ import emailjs from 'emailjs-com';
 import apiKeys from './apiKeys';
 
 const Footer = () => {
-  const bodyObj = {
-			from_name: '',
-			to_name: 'Kara',
-			user_email: '',
-			message: '',
-  };
-
-  const [text, setText] = useState(bodyObj);
-	const [modal, setModal] = useState(false);
-
-	 const toggle = () => setModal(!modal);
-
-		const handleSubmit = (e) => {
-			e.preventDefault();
-			text !== bodyObj
-				? emailjs
-						.send(apiKeys.SERVICE, apiKeys.TEMPLATE_ID, text, apiKeys.USER_ID)
-						.then((res) => {
-							console.log('SUCCESS!', res.status, res.text);
-						})
-						.then((res) => {
-							setText(bodyObj);
-						})
-						.catch((err) => {
-							console.log('FAILED...', err);
-						})
-				: console.log('No Information Supplied');
-			toggle()
-		};
-
-		const handleChange = (e) => {
-			setText({ ...text, [e.target.name]: e.target.value });
-    };
+//  const bodyObj = {
+//			from_name: '',
+//			to_name: 'Kara',
+//			user_email: '',
+//			message: '',
+//  };
+//
+//  const [text, setText] = useState(bodyObj);
+//	const [modal, setModal] = useState(false);
+//
+//	 const toggle = () => setModal(!modal);
+//
+//		const handleSubmit = (e) => {
+//			e.preventDefault();
+//			text !== bodyObj
+//				? emailjs
+//						.send(apiKeys.SERVICE, apiKeys.TEMPLATE_ID, text, apiKeys.USER_ID)
+//						.then((res) => {
+//							console.log('SUCCESS!', res.status, res.text);
+//						})
+//						.then((res) => {
+//							setText(bodyObj);
+//						})
+//						.catch((err) => {
+//							console.log('FAILED...', err);
+//						})
+//				: console.log('No Information Supplied');
+//			toggle()
+//		};
+//
+//		const handleChange = (e) => {
+//			setText({ ...text, [e.target.name]: e.target.value });
+//    };
       const twitter = 'https://twitter.com/KaraPeoples_Dev';
-			const linkedIn = 'https://www.linkedin.com/in/karapeoples/';
+	  const linkedIn = 'https://www.linkedin.com/in/karapeoples/';
   return (
 			<div>
 				<Navbar className='nav-bar'>
@@ -50,38 +50,38 @@ const Footer = () => {
 							<Button target='_blank' rel='noopener noreferrer' className='c-btn' href={linkedIn}>
 								<img src='https://img.icons8.com/fluent-systems-regular/50/000000/linkedin.png' alt='linkedIn' /> LinkedIn
 							</Button>
-//							<Button onClick={toggle} className='c-btn'>
-//								<img src='https://img.icons8.com/carbon-copy/100/000000/email.png' alt='email'/> Email Me
-//							</Button>
+
 						</div>
 						<div>
 							<h5 className='copyright'>©2022~~Kara R. Peoples </h5>
 						</div>
 					</Nav>
 				</Navbar>
-				<Modal isOpen={modal}>
-					<ModalHeader toggle={toggle}>Contact Me</ModalHeader>
-					<ModalBody>
-						<form id='contact-form' onSubmit={handleSubmit}>
-							<label>Name</label>
-							<input placeholder='Name' type='text' name='from_name' onChange={handleChange} value={text.from_name} />
-							<br />
-							<label>Email</label>
-							<input placeholder='Email..' type='email' name='user_email' onChange={handleChange} value={text.user_email} />
-							<br />
-							<label>Message</label>
-							<textarea
-								placeholder='Write me a Note...'
-								type='text'
-								name='message'
-								onChange={handleChange}
-								value={text.message}
-							/>
-							<Button className='s-btn'>Send</Button>
-						</form>
-					</ModalBody>
-				</Modal>
 			</div>
+
+//				<Modal isOpen={modal}>
+//					<ModalHeader toggle={toggle}>Contact Me</ModalHeader>
+//					<ModalBody>
+//						<form id='contact-form' onSubmit={handleSubmit}>
+//							<label>Name</label>
+//							<input placeholder='Name' type='text' name='from_name' onChange={handleChange} value={text.from_name} />
+//							<br />
+//							<label>Email</label>
+//							<input placeholder='Email..' type='email' name='user_email' onChange={handleChange} value={text.user_email} />
+//							<br />
+//							<label>Message</label>
+//							<textarea
+//								placeholder='Write me a Note...'
+//								type='text'
+//								name='message'
+//								onChange={handleChange}
+//								value={text.message}
+//							/>
+//							<Button className='s-btn'>Send</Button>
+//						</form>
+//					</ModalBody>
+//				</Modal>
+
 		);
 }
 
